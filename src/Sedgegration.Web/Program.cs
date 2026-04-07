56eb7537bad3933b089c6d4a89175b3139427899
alive <aliversidge89@gmail.com>
2026-04-07 17:03:38 +0100

    test

--- src/Sedgegration.Web/Program.cs
+++ src/Sedgegration.Web/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddHttpClient<ManagementApiClient>(http =>
         ?? "http://localhost:5000";
     http.BaseAddress = new Uri(baseAddress);
 });
-
+//testing
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
